74875a83afb88e3efcd17e1827b368f7eab4c0c6
whd <553670407@qq.com>
2019-01-26 17:30:47 +0800

    20190126

--- src/main/java/com/whd/springBoot/HelloWorld.java
+++ src/main/java/com/whd/springBoot/HelloWorld.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-	
-    @RequestMapping("/hello")
-    public String home() {
-        return "Hello World!";
-    }
 
+	@RequestMapping("/hello")
+	public String home() {
+		System.out.println("lalala");
+		return "Hello World!";
+	}
 
 }
--- src/main/java/com/whd/springBoot/MyImageController.java
+++ src/main/java/com/whd/springBoot/MyImageController.java
@@ -22,23 +22,27 @@ import com.whd.util.FtpUtil;
 
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
+
 @Controller
 public class MyImageController {
 	@Autowired
 	ImageService imgService;
-	@RequestMapping(value="/upLoadImg")
+
+	@RequestMapping(value = "/upLoadImg")
 	public String upLoadImg() {
 		return "image";
 	}
-	@RequestMapping(value="/inserImg", method = RequestMethod.POST)
-	public void inserImg( HttpServletRequest request,HttpServletResponse response, @RequestParam("img") MultipartFile file ) throws IOException {
+
+	@RequestMapping(value = "/inserImg", method = RequestMethod.POST)
+	public void inserImg(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam("img") MultipartFile file) throws IOException {
 		try {
 			request.setCharacterEncoding("utf-8");
-			String id  = request.getParameter("id");
+			String id = request.getParameter("id");
 			System.out.println(id);
-			if(!file.isEmpty()){
+			if (!file.isEmpty()) {
 				BASE64Encoder encoder = new BASE64Encoder();
-				//通过base64来转化图片
+				// 通过base64来转化图片
 				String img = encoder.encode(file.getBytes());
 				Image record = new Image();
 				record.setId(id);
@@ -50,27 +54,31 @@ public class MyImageController {
 			e.printStackTrace();
 		}
 	}
-	//上传图片到FTP
-	@RequestMapping(value="/inserImgFtp", method = RequestMethod.POST)
-	public void inserImgFtp( HttpServletRequest request,HttpServletResponse response, @RequestParam("img") MultipartFile file ) throws IOException {
+
+	// 上传图片到FTP
+	@RequestMapping(value = "/inserImgFtp", method = RequestMethod.POST)
+	public void inserImgFtp(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam("img") MultipartFile file) throws IOException {
 		InputStream inputStream = file.getInputStream();
-		//家里IP:192.168.1.4
+		// 家里IP:192.168.1.4
 		FtpUtil.uploadFile("172.20.10.4", 21, "newuser", "123456", "/", "/2016/11", "123.jpg", inputStream);
-	}	//从FTP上下载图片并转化为Base64
-	@RequestMapping(value="/getImgFtp", method = RequestMethod.GET)
+	} // 从FTP上下载图片并转化为Base64
+
+	@RequestMapping(value = "/getImgFtp", method = RequestMethod.GET)
 	public Response getImgFtp() throws IOException {
-		
-		return FtpUtil.downloadFile("192.168.1.4", 21, "newuser", "123456",  "/sad/2016/11", "211.jpg", "I:/mylocalftp");
+
+		return FtpUtil.downloadFile("192.168.1.4", 21, "newuser", "123456", "/sad/2016/11", "211.jpg", "I:/mylocalftp");
 	}
-	@RequestMapping(value="/getImg", method = RequestMethod.GET)
-	public void getImg( HttpServletRequest request,HttpServletResponse response, String id ) throws IOException {
+
+	@RequestMapping(value = "/getImg", method = RequestMethod.GET)
+	public void getImg(HttpServletRequest request, HttpServletResponse response, String id) throws IOException {
 		Image img = imgService.select(id);
-		byte[] byteAry = (byte[])img.getImg();
+		byte[] byteAry = (byte[]) img.getImg();
 		String data = new String(byteAry, "utf-8");
 		BASE64Decoder decoder = new BASE64Decoder();
 		byte[] bytes = decoder.decodeBuffer(data);
 		for (int i = 0; i < bytes.length; i++) {
-			if(bytes[i] < 0){
+			if (bytes[i] < 0) {
 				bytes[i] += 256;
 			}
 		}
